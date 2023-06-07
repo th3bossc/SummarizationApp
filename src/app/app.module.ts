f4dbb6e1645f7bb49ea9977b61baff2c42ee70f6
Th3BossC <diljith2003@gmail.com>
2023-06-07 11:58:50 +0400

    added beta pwa functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -24,6 +24,7 @@ import { MessageService } from 'primeng/api';
 import { HttpClientModule } from '@angular/common/http';
 import { ApiCallService } from './Services/api-call.service';
 import { ThemeService } from './Services/theme.service';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -49,7 +50,13 @@ import { ThemeService } from './Services/theme.service';
     InputTextareaModule,
     AnimateModule,
     ToolbarModule,
-    FieldsetModule
+    FieldsetModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [
     MessageService, 
